1fc6bbd6ddb7470ffd0127be27d16aef2c6753ff
shigenaga.k <kshigenaga5@gmail.com>
2021-02-11 13:44:07 +0900

    fix: 4.パドルとキーボード操作

--- breakBlock.js
+++ breakBlock.js
@@ -9,9 +9,23 @@ var dy = -2;
 
 var ballRadius = 10;
 
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width - paddleWidth) / 2;
+var rightPressed = false;
+var leftPressed = false;
+
 function drawBall(){
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#0095dd";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawPaddle(){
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
     ctx.fillStyle = "#0095dd";
     ctx.fill();
     ctx.closePath();
@@ -21,6 +35,8 @@ function draw(){
     //描画コード
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    drawPaddle();
+
     x += dx;
     y += dy;
 
@@ -31,6 +47,28 @@ function draw(){
     if(y + dy > canvas.height-ballRadius || y + dy < 0 + ballRadius){
         dy = -dy;
     }
+
+    if(rightPressed && paddleX < canvas.width-paddleWidth){
+        paddleX += 7;
+    }else if(leftPressed && paddleX > 0){
+        paddleX -= 7;
+    }
 }
 
+document.addEventListener("keydown", keyDownHandler, false);
+function keyDownHandler(e){
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    } else if(e.key = "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+}
+document.addEventListener("keyup", keyUpHandler, false);
+function keyUpHandler(e){
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    } else if(e.key = "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+}
 setInterval(draw, 10);
