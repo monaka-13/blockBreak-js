f27361ab194975bd2b4860df13fc9cc321b5e50a
shigenaga.k <kshigenaga5@gmail.com>
2021-02-11 17:47:26 +0900

    fix: 7バグ修正

--- breakBlock.js
+++ breakBlock.js
@@ -121,13 +121,27 @@ function keyUpHandler(e){
 function collisionDetection(){
     for (var c = 0; c < brickColumnCount; c++){
         for(var r = 0; r < brickRowCount; r++){
-            var b=bricks[c][r];
-            if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
-                dy = -dy;
-                b.status = 0;
+            var b =bricks[c][r];
+            if(b.status == 1){
+                if(x > b.x && x < b.x+brickWidth && y > b.y && y < b. y+brickHeight) {
+                    dy = -dy;
+                    b.status = 0;
+                    score++;
+                    if(score == brickRowCount*brickColumnCount){
+                        alert("YouWin");
+                        document.location.reload();
+                        clearInterval(interval);
+                    }
+                }
             }
         }
     }
+}
+
+function drawScore(){
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095dd";
+    ctx.fillText("Score: "+score, 8, 20);
 }
 
 var interval = setInterval(draw, 10);
